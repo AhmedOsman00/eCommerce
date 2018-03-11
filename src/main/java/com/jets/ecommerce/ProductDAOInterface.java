9f4033d79e2b93f53a7f6b17e598a4c3464aee44
Ahmed Osman <osman01191@gmail.com>
2018-03-11 23:21:34 +0200

    First Version

---
+++ src/main/java/com/jets/ecommerce/ProductDAOInterface.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jets.ecommerce;
+
+import com.jets.dao.Product;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ahmed
+ */
+public interface ProductDAOInterface {
+
+    void insertProduct(Product product);
+    void deleteProduct(Product product);
+    ArrayList<Product> selectAllProducts();
+    
+}
